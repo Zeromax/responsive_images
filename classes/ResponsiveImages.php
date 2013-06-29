6003efdc0f864388cf9e64d920ba00275e68a2d9
Zeromax <zero@brothers-project.de>
2013-06-30 01:25:28 +0200

    added regex for detecting img tags and resize them

--- classes/ResponsiveImages.php
+++ classes/ResponsiveImages.php
@@ -26,7 +26,7 @@ namespace Contao;
 class ResponsiveImages
 {
 	/**
-	 * Hook to override teh max Image Width global
+	 * Hook to override the max Image Width global
 	 * @param \Template $objTemplate
 	 */
 	public function overrideImageSize($objTemplate)
@@ -34,6 +34,73 @@ class ResponsiveImages
 		if (TL_MODE == "FE")
 		{
 			$GLOBALS['TL_CONFIG']['maxImageWidth'] = self::getBreakpoint();
+			$GLOBALS['TL_CONFIG']['elementToResizeImg'][] = array ('ce_text', 'text');
+
+			if($GLOBALS['TL_CONFIG']['maxImageWidth'] > 0 && isset($GLOBALS['TL_CONFIG']['elementToResizeImg']) && is_array($GLOBALS['TL_CONFIG']['elementToResizeImg']))
+			{
+				foreach ($GLOBALS['TL_CONFIG']['elementToResizeImg'] as $element)
+				{
+					if($objTemplate->getName() == $element[0])
+					{
+						switch ($element[1])
+						{
+							case 'text':
+								// pattern for IMG Tags
+								$pattern = '/\<img[a-z0-9%#:&;\/\.\_\-\="\s]*>/i';
+								$subject = $element[1];
+								$matches = "";
+								preg_match_all($pattern, $objTemplate->$subject, $matches);
+
+								// Replace each file if necessary
+								foreach($matches[0] as $file)
+								{
+									// Pattern for height| width | src
+									$imgPattern = '/(src="(?<file>(files|tl_files)[a-z0-9%\/\s\.\-\_]{0,250})")|(width="(?<width>[0-9]{1,9})")|(height="(?<height>[0-9]{1,9})")/i';
+									$fileMatches = "";
+									preg_match_all($imgPattern, $file, $fileMatches);
+
+									$tmpFile =  array_filter($fileMatches['file']);
+									sort($tmpFile);
+									$resizeFile = $tmpFile[0];
+
+									$path = "";
+									if (TL_FILES_URL != '')
+									{
+										$path = TL_FILES_URL . $GLOBALS['TL_CONFIG']['uploadPath'] . '/';
+										$resizeFile = str_replace($path, "", $resizeFile);
+									}
+									if (file_exists(TL_ROOT .'/'. rawurldecode($resizeFile)))
+									{
+										$intMaxWidth = $GLOBALS['TL_CONFIG']['maxImageWidth'];
+										$imgSize = @getimagesize(TL_ROOT .'/'. rawurldecode($resizeFile));
+										if($imgSize[1] > $intMaxWidth || $imgSize[0] > $intMaxWidth)
+										{
+											// Adjust the image size
+											$ratio = $imgSize[1] / $imgSize[0];
+
+											$size[0] = $intMaxWidth;
+											$size[1] = floor($intMaxWidth * $ratio);
+											$src = \Image::get($resizeFile, $size[0], $size[1], '');
+											$replaceFile = str_replace( 'src="'.$tmpFile[0], 'src="'.$path.$src, $file );
+											$replaceFile = str_replace( rawurldecode($tmpFile[0]), $path.$src, $replaceFile );
+											$tmpWidth =  array_filter($fileMatches['width']);
+											sort($tmpWidth);
+											$replaceFile = str_replace( 'width="'.$tmpWidth[0], 'width="'.$size[0], $replaceFile );
+											$tmpHeight =  array_filter($fileMatches['height']);
+											sort($tmpHeight);
+											$replaceFile = str_replace( 'height="'.$tmpHeight[0], 'height="'.$size[1], $replaceFile );
+
+											$objTemplate->$subject = str_replace( $file, $replaceFile, $objTemplate->$subject );
+										}
+									}
+								}
+								break;
+							default:
+								break;
+						}
+					}
+				}
+			}
 		}
 	}
 
