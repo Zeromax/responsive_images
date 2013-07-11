1fec38f87288ff7eabb3336e0229e73d6fc4ded9
Zeromax <zero@brothers-project.de>
2013-07-12 00:36:55 +0200

    added forceResponsiveCookie for Development set the Resolution Cookie with the view port every page reload

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -15,7 +15,7 @@
  * System configuration
  */
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('maxImageWidth', 'breakPoints,mobileWidthFallback', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('maxImageWidth', 'breakPoints,mobileWidthFallback,forceResponsiveCookie', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 $GLOBALS['TL_DCA']['tl_settings']['fields']['breakPoints'] = array
 (
 	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['breakPoints'],
@@ -27,4 +27,10 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['mobileWidthFallback'] = array
 	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'],
 	'inputType'               => 'text',
 	'eval'                    => array('tl_class'=>'w50', 'rgxp'=>'digit')
+);
+$GLOBALS['TL_DCA']['tl_settings']['fields']['forceResponsiveCookie'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['forceResponsiveCookie'],
+	'inputType'               => 'checkbox',
+	'eval'                    => array('tl_class'=>'w50')
 );
