6ba18b87a38435b315768759b824f82d8248e382
Zeromax <zero@brothers-project.de>
2013-07-12 02:14:30 +0200

    added mobile text for a better understanding

--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -13,6 +13,6 @@
 $GLOBALS['TL_LANG']['tl_settings']['breakPoints'][0] = "Breakpoint [responsive_images]";
 $GLOBALS['TL_LANG']['tl_settings']['breakPoints'][1] = "Definieren Sie die Bildschirmbreiten, bei welcher Auflösung ein kleineres Bild erstellt werden soll. (Bsp.: 600,800,1200)";
 $GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][0] = "Fallback für Bildschirmbreite [responsive_images]";
-$GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][1] = "Sollte kein Cookie erlaubt sein oder Javascript deaktiviert, wird dieser Auflösung als Fallback Verwendet.";
+$GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][1] = "Sollte kein Cookie erlaubt sein oder Javascript deaktiviert, wird diese Auflösung bei Mobilgeräten als Fallback Verwendet.";
 $GLOBALS['TL_LANG']['tl_settings']['forceResponsiveCookie'][0] = "Auflösungs Cookie [responsive_images]";
 $GLOBALS['TL_LANG']['tl_settings']['forceResponsiveCookie'][1] = "Erstellt jedesmal das Cookie mit der Auflösung und verwendet die Viewport breite. Nur zum testen Aktivieren!";
--- languages/en/tl_settings.php
+++ languages/en/tl_settings.php
@@ -13,6 +13,6 @@
 $GLOBALS['TL_LANG']['tl_settings']['breakPoints'][0] = "Breakpoint [responsive_images]";
 $GLOBALS['TL_LANG']['tl_settings']['breakPoints'][1] = "Define the screen widths on wich a smaller Image should be created. (e.g.: 600,800,1200)";
 $GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][0] = "Screen width Fallback [responsive_images]";
-$GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][1] = "If cookies or Javascript is not allowed than we use this Resolution as Fallback.";
+$GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][1] = "If cookies or Javascript is not allowed than we use this Resolution for mobiles as Fallback.";
 $GLOBALS['TL_LANG']['tl_settings']['forceResponsiveCookie'][0] = "Resolution Cookie [responsive_images]";
 $GLOBALS['TL_LANG']['tl_settings']['forceResponsiveCookie'][1] = "Create the Resolution Cookie every time and uses the viewport width. Activate this only for testing!";
