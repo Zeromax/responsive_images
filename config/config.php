7d4d5d421d9dac7526d0077f9809b61e933abedb
zeromax <zero@brothers-project.de>
2014-01-26 23:24:24 +0100

    clean up old code

--- config/config.php
+++ config/config.php
@@ -9,7 +9,3 @@
  * @author    Andreas Nölke
  * @copyright Andreas Nölke 2013
  */
-
-$GLOBALS['TL_HOOKS']['initializeSystem'][] = array('ResponsiveImages', 'setCookie');
-$GLOBALS['TL_HOOKS']['modifyFrontendPage'][] = array('ResponsiveImages', 'replaceImages');
-$GLOBALS['TL_HOOKS']['getCacheKey'][] = array('ResponsiveImages', 'overrideCacheKey');
