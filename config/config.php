2e9278164b69d5010bd9cd724fa83b7a0c54bdd7
zeromax <zero@brothers-project.de>
2014-01-26 23:30:23 +0100

    added picturefill-1.2.1 to the front end

--- config/config.php
+++ config/config.php
@@ -9,3 +9,9 @@
  * @author    Andreas Nölke
  * @copyright Andreas Nölke 2013
  */
+// add needed picturefill JavaScript for the Frontend
+if (TL_MODE == "FE")
+{
+	$GLOBALS['TL_JAVASCRIPT'][] = "system/modules/responsive_images/assets/picturefill-1.2.1/matchmedia.js|static";
+	$GLOBALS['TL_JAVASCRIPT'][] = "system/modules/responsive_images/assets/picturefill-1.2.1/picturefill.js|static";
+}
