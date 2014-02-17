83cb49ca943729dbfe90994678757043ea3fbfe9
zeromax <zero@brothers-project.de>
2014-02-18 00:21:52 +0100

    removed unused label

--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -14,5 +14,3 @@ $GLOBALS['TL_LANG']['tl_settings']['breakPoints'][0] = "Breakpoint [responsive_i
 $GLOBALS['TL_LANG']['tl_settings']['breakPoints'][1] = "Definieren Sie die Bildschirmbreiten, bei welcher Auflösung ein kleineres Bild erstellt werden soll. (Bsp.: 600,800,1200)";
 $GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][0] = "Fallback für Bildschirmbreite [responsive_images]";
 $GLOBALS['TL_LANG']['tl_settings']['mobileWidthFallback'][1] = "Sollte kein Cookie erlaubt sein oder Javascript deaktiviert, wird diese Auflösung bei Mobilgeräten als Fallback Verwendet.";
-$GLOBALS['TL_LANG']['tl_settings']['forceResponsiveCookie'][0] = "Nutze Viewport Breite [responsive_images]";
-$GLOBALS['TL_LANG']['tl_settings']['forceResponsiveCookie'][1] = "Erstellt jedesmal das Cookie mit der Auflösung und verwendet die Viewport breite. Nur zum testen Aktivieren!";
